0b2b60fe6f40df7820076340721c4df3d41ab432
David Vescovi <dvescovi@tampabay.rr.com>
2025-06-26 16:52:19 -0400

    high speed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -249,7 +249,7 @@ static void MX_TIM15_Init(void)
   */
   GPIO_InitStruct.Pin = LL_GPIO_PIN_5|LL_GPIO_PIN_6;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
   GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
